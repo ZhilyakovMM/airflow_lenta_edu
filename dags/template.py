5120f4e825a9a1d2c88c93477e65ff3910ef2c58
Жиляков Михаил <mikhail.zhilyakov@glowbyteconsulting.com>
2024-11-15 12:02:38 +0300

    Init

---
+++ dags/template.py
@@ -0,0 +1,47 @@
+import datetime
+import os
+import sys
+import pendulum
+import logging
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.exceptions import AirflowSkipException
+
+DAG_NAME = os.path.basename(__file__).replace('.pyc', '').replace('.py', '')
+# Add python path
+PYTHON_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__))).replace('\\', '/')
+sys.path.append(PYTHON_PATH)
+
+dag_default_args = {
+    'owner': 'Zhilyakov Mikhail',
+    'email': ['mikhail.zhilyakov@glowbyteconsulting.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 5,
+    'retry_delay': datetime.timedelta(minutes=5)
+}
+
+dag = DAG(
+    dag_id=DAG_NAME,
+    default_args=dag_default_args,
+    max_active_runs=1,
+    # Регламент каждые 3 часа
+    schedule_interval="0 */3 * * *",
+    start_date=pendulum.datetime(2023, 3, 21, tz="UTC"),
+    catchup=False,
+    tags=["IDC_STG_ODS", "APP_METR"]
+)
+
+def example(**kwargs):
+    print(f'Template. Kwargs: {kwargs}')
+
+
+example_op = PythonOperator(
+    dag=dag,
+    task_id=f"{example.__name__}",
+    do_xcom_push=True,
+    python_callable=example,
+    op_kwargs={'example_kwarg': 'example'},
+    provide_context=True
+)
