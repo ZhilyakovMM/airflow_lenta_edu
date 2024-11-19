cd86e1a7c86c6309b10f04a69c26bfec7219297f
Жиляков Михаил <mikhail.zhilyakov@glowbyteconsulting.com>
2024-11-19 13:17:06 +0300

    Fix

--- src/spark_app.py
+++ src/spark_app.py
@@ -1,15 +1,16 @@
 import sys
 from pyspark.sql import SparkSession
 
-schema_table_name = sys.argv[1]
-user = sys.argv[2]
+schema_name = sys.argv[1]
+table_name = sys.argv[2]
+user = sys.argv[3]
 
 spark = SparkSession.builder \
-                    .appName(f"CTAS_{schema_table_name}") \
+                    .appName(f"CTAS_{schema_name}_{table_name}_{user}") \
                     .enableHiveSupport() \
                     .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")\
                     .getOrCreate()
 
-df = spark.sql(f"SELECT * FROM {schema_table_name} LIMIT 100;")
-df.write.format("iceberg").saveAsTable(f"spark__{schema_table_name}__user")
+df = spark.sql(f"SELECT * FROM {schema_name}.{table_name} LIMIT 100;")
+df.write.format("iceberg").saveAsTable(f"{schema_name}.{table_name}__{user}")
 
