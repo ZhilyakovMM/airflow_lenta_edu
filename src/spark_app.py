9cc73eb02a84d9d50b8881aa7a864d1dd6694e41
Жиляков Михаил <mikhail.zhilyakov@glowbyteconsulting.com>
2024-11-19 14:36:19 +0300

    Fix

--- src/spark_app.py
+++ src/spark_app.py
@@ -12,10 +12,10 @@ spark = SparkSession.builder \
                     .getOrCreate()
 
 df = spark.sql(f"SELECT * FROM {schema_name}.{table_name} LIMIT 100;")
-df.writeTo(f"{schema_name}.spark__{table_name}__{user}") \
-    .tableProperty("write.parquet.compression-codec", "snappy") \
-    .tableProperty("write.format.default", "orc") \
-    .partitionedBy("name") \
-    .using("iceberg") \
-    .create()
+# df.writeTo(f"{schema_name}.spark__{table_name}__{user}") \
+#     .tableProperty("write.parquet.compression-codec", "snappy") \
+#     .tableProperty("write.format.default", "orc") \
+#     .partitionedBy("name") \
+#     .using("iceberg") \
+#     .create()
 
